f25cae93b163cef7dd805a12533add07a6bbbc56
Mona Lisa Mameli <lisa.noetzel@gmail.com>
2019-09-23 20:06:20 -0400

    pseudo-coding approved

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+//Require MySqL & Inquirer - node npm intallation pages (use console.table) package
+
+//Show MySQL Products table w/ 10 items - use console.log to show the table data
+
+//Generate an automatic message A: "Which product would you like to purchase? Please input its 5 digit ID number. How many would you like?"- that is an Inquirer prompt
+
+
+// Compare user input to pre-established item's quantity in table (use  < > =  with if else statement? ) 
+
+//if purchaseSuccessfull , subtract User order from pre-established quantity and) update MySQL db &  message 1: "Thank you ! Your order has been processed !"
+
+// or else generate message 2: "I'm sorry, that quantity is not available" 
+
+// after either scenario, message prompt "Would you like to purchase something else? Would you like to leave?" return to message A
+
+
+
+//10.25 + 10.26 + 12.10
+
+
