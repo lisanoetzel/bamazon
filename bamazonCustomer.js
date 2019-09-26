d62d1491c52253f1247433859b9f9ba20fbbed29
Mona Lisa Mameli <lisa.noetzel@gmail.com>
2019-09-25 23:15:57 -0400

    initial coding of js file; set-up bAmazon db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,53 @@
-//Require MySqL & Inquirer - node npm intallation pages (use console.table) package
+//Require MySqL & Inquirer packages - node npm intallation pages (use console.table) 
+    var inquirer = require("inquirer");
+    var mysql = require("mysql");
+    var consoleTable = require("console.table");
+        console.log("Hello! Welcome to bAmazon!");
+
+//Connecting to MySql database
+    var connection = mysql.createConnection({
+        host: "LocalHost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "bAmazon_DB"
+    });
+
+    connection.connect(function(err){
+        if (err) throw err;
+        console.log("You are listening on " + connection.port);
+    })
 
 //Show MySQL Products table w/ 10 items - use console.log to show the table data
 
-//Generate an automatic message A: "Which product would you like to purchase? Please input its 5 digit ID number. How many would you like?"- that is an Inquirer prompt
+//Inquirer Prompt: generating automatic messages: A= "Which product would you like to purchase? Please input its 5 digit ID number. B= How many would you like?
 
+    inquirer.prompt ([
+        {
+            type: "input",
+            message: "Which product would you like? Please input the item's 5 digit ID number.",
+            name: "itemID"
+        },
+        {
+            type: "input",
+            message: "How many would you like?",
+            name: "itemAMT" 
+        },
+        {
+            type: "confirm",
+            message: "Would you like to purchase something else?",
+            name: "confirm",
+            default: true
+        }
+    ])
+        .then(function(inquirerResponse){
+            if (inquirerResponse.itemAMT >= itemTableAmt) {
+                console.log("Thank you! Your order has been processed!" + inquirerResponse.confirm)
+            }
+            else {
+                console.log("I'm sorry, that quantity is not available.")
+            }
+        })
 
 // Compare user input to pre-established item's quantity in table (use  < > =  with if else statement? ) 
 
